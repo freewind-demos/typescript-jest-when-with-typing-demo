a0527bb9db7e8b24ba575f6fe0ff456a3eae1f3c
Freewind <nowindlee@gmail.com>
2020-09-26 15:39:14 +0800

    typescript-jest-when-with-typing-demo

--- README.md
+++ README.md
@@ -1,29 +1,7 @@
-TypeScript Jest Mock Module with Exported Default Function Demo
+TypeScript Jest "when" with Typing Demo
 =========================================================
 
-如果一个module定义类似于：
-
-```
-export default function foo() {}
-```
-
-在jest中怎么来mock这个函数？
-
-经过测试，应该是:
-
-```
-jest.mock('./foo', () => jest.fn())
-```
-
-而不是我本来以为的：
-
-```
-jest.mock('./foo', () => ({
-  default: jest.fn()
-}))
-```
-
-再结合一个自定义的强转函数`asMock`，可以让我们得到一个强类型的mock，方便后面操作。
+`jest-when`可以搭配jest中定义好的mock类型，如果类型不对，会编译报错。
 
 ```
 npm install
--- hello.test.ts
+++ hello.test.ts
@@ -1,6 +1,7 @@
 import asMock from "./asMock";
 import decorate from './decorate';
 import hello from "./hello";
+import {when} from 'jest-when';
 
 // Note: we just return a `jest.fn()` for the default exported function,
 // not `{ default: jest.fn() }`
@@ -11,16 +12,15 @@ describe('mock test', () => {
 
   beforeEach(mockDecorate.mockClear);
 
-  it('test1', () => {
-    mockDecorate.mockImplementation((s) => `[${s}]`)
-    expect(hello('aaa')).toEqual('Hello, [aaa]!')
-    expect(mockDecorate).toHaveBeenCalledWith('aaa')
+  it('good', () => {
+    when(mockDecorate).calledWith('aaa').mockReturnValue('AAA');
+    expect(hello('aaa')).toEqual('Hello, AAA!')
+
+    expect(hello('others')).toEqual('Hello, undefined!')
   })
 
-  it('test2', () => {
-    mockDecorate.mockImplementation((s) => `<${s}>`)
-    expect(hello('bbb')).toEqual('Hello, <bbb>!')
-    expect(mockDecorate).toHaveBeenCalledWith('bbb')
+  it('typing failing', () => {
+    when(mockDecorate).calledWith(123).mockReturnValue(456);
   })
 
 })
